47bff3d01caefa57730e9a92f68d3d361fa0c819
ships22 <sshipsss@gmail.com>
2021-01-17 13:31:09 +0100

    Middleware added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import products from './data/products.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -11,13 +12,14 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Api is running.....')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+
+app.use('/api/products/', productRoutes)
+
+// @desc custom error middleware
+// @route all
+// @acess Public
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
