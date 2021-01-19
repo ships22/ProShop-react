e76d8a23ecc7b00be6053fc497e379260aca1c3d
ships22 <sshipsss@gmail.com>
2021-01-19 18:08:22 +0100

    ProductList Reducer bug fix

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -7,21 +7,32 @@ import { listProducts } from '../actions/productActions'
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+  console.log('test products here', products)
+
   useEffect(() => {
     dispatch(listProducts())
   }, [dispatch])
 
-  const products = []
+  // const products = []
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
