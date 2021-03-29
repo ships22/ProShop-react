632ed00a98dd9ca5bce09486be4e1b33d90a4b57
ships22 <sshipsss@gmail.com>
2021-03-30 00:36:01 +0200

    Order controller and route added

--- client/src/screens/PlaceOrderScreen.js
+++ client/src/screens/PlaceOrderScreen.js
@@ -9,12 +9,19 @@ const PlaceOrderScreen = () => {
   const cart = useSelector((state) => state.cart)
 
   // Calculate prices
-  cart.itemsPrice = cart.cartItems.reduce(
-    (acc, item) => acc + item.price * item.qty,
-    0
+  const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+  }
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   )
-  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 100
-  cart.taxPrice = Number((0.15 * cart.itemsPrice).toFixed(2))
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
   const placeOrderHandler = () => {
     console.log('order')
   }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products/', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 // @desc custom error middleware
 // @route all
